bd44ad03f31519e562193c1e810b43781468ce34
YanMorera <90201379+YanMorera@users.noreply.github.com>
2023-06-13 14:56:08 -0500

    Class: 185

--- controllers/search.controller.js
+++ controllers/search.controller.js
@@ -1,5 +1,40 @@
 import { request, response } from 'express'
+import { Types } from 'mongoose'
+import { User, Category, Product } from '../models/index.js'
+const allowedCollections = ['users', 'categories', 'products', 'roles']
 
-export const search = (req = request, res = response) => {
-    res.json({ msg: 'Buscar' })
+const searchUsers = async (criteria, res = response) => {
+    const isMongoId = Types.ObjectId.isValid(criteria)
+    if (isMongoId) {
+        const user = await User.findById(criteria)
+        return res.json({
+            results: user ? [user] : [],
+        })
+    }
+}
+
+export const search = async (req = request, res = response) => {
+    const { collection, criteria } = req.params
+
+    if (!allowedCollections.includes(collection)) {
+        return res.status(400).json({
+            msg: `Allowed collections are: ${allowedCollections}`,
+        })
+    }
+
+    switch (collection) {
+        case 'users':
+            await searchUsers(criteria, res)
+            break
+        case 'categories':
+            break
+        case 'products':
+            break
+        case 'roles':
+            break
+        default:
+            return res.status(500).json({
+                msg: 'Theres no have behavior define to do this',
+            })
+    }
 }
